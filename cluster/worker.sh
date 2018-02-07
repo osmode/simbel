098c0f98b9a74297064ceeb8dd15747fff882a87
Omar Metwally <omar.metwally"gmail.com>
2018-02-07 03:51:53 +0000

    add worker.sh

---
+++ cluster/worker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script to initialize a worker node in a 
+# Raspberry Pi cluster
+# Modified from: http://www.instructables.com/id/How-to-Make-a-Raspberry-Pi-SuperComputer/
+# Author: Omar N. Metwally, MD
+# omar.metwally@gmail.com
+# https://github.com/osmode/simbel
+# USAGE:  ./worker.sh 
+echo We now need to create a new ssh keypair.
+ssh-keygen -b 2048 -t rsa
+cp /home/pi/.ssh/id_rsa.pub pi01
+ssh pi@10.0.0.243
+ssh-keygen -b 2048 -t rsa
+cp /home/pi/.ssh/id_rsa.pub pi02
+scp 10.0.0.26:/home/pi/.ssh/pi01
+cat /home/pi/.ssh/pi01 >> authorized_keys
+exit
+
+cat /home/pi/.ssh/pi01 /home/pi/.ssh/pi02 >> /home/pi/.ssh/authorized_keys
+
+
