9589d55b049ba172b46a9e919dc7c89243bbc55d
Omar Metwally <omar.metwally@gmail.com>
2018-02-08 21:21:37 +0000

    add cluster.sh

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -8,17 +8,19 @@
 # USAGE:  ./worker.sh 
 echo We now need to create a new ssh keypair.
 ssh-keygen -b 2048 -t rsa
-cp /home/pi/.ssh/id_rsa.pub pi01
-ssh pi@10.0.0.243
-ssh-keygen -b 2048 -t rsa
-cp /home/pi/.ssh/id_rsa.pub pi02
-scp 10.0.0.26:/home/pi/.ssh/pi01
-ssh pi@10.0.0.233
-ssh-keygen -b 2048 -t rsa
-cp /home/pi/.ssh/id_rsa.pub pi03
+# CHANGE pi04 below to the corresponding raspberry pi node in your cluster
+cp /home/pi/.ssh/id_rsa.pub pi04
+
+# copy public keys from all rasperry pi's in the cluster
+scp 10.0.0.26:/home/pi/.ssh/pi01 /home/pi/.ssh/pi01
+scp 10.0.0.243:/home/pi/.ssh/pi02 /home/pi/.ssh/pi02
+scp 10.0.0.233:/home/pi/.ssh/pi03 /home/pi/.ssh/pi03
+scp 10.0.0.142:/home/pi/.ssh/pi04 /home/pi/.ssh/pi04
+
+# add all pis' public keys to authorized_keys
 cat /home/pi/.ssh/pi01 >> authorized_keys
 cat /home/pi/.ssh/pi02 >> authorized_keys
 cat /home/pi/.ssh/pi03 >> authorized_keys
+cat /home/pi/.ssh/pi04 >> authorized_keys
 
 exit
-
